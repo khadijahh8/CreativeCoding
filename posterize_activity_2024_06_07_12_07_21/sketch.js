1cfcc2e34479de4525da92aef72488e05794a56e
khadijahh8 <144089783+khadijahh8@users.noreply.github.com>
2024-06-07 16:08:41 +0400

    posterize_activity

---
+++ posterize_activity_2024_06_07_12_07_21/sketch.js
@@ -0,0 +1,39 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
+
+var img;
+
+function preload() {
+
+img = loadImage("girl.jpg");
+
+}
+
+
+
+function setup () {
+
+createCanvas (400, 400);
+
+background(0);
+
+}
+
+
+
+function draw() {
+
+background(0);
+
+image(img, 0, 0);
+
+var v = map(mouseX, 0, width, 2, 20);
+
+filter(POSTERIZE, v);
+
+}
