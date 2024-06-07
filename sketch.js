f5fa7ba4ecfc8c9c98c54093ca1831b2ff41d73a
khadijahh8 <144089783+khadijahh8@users.noreply.github.com>
2024-06-07 15:27:31 +0400

    Alien activity

--- sketch.js
+++ sketch.js
@@ -1,56 +1,60 @@
+//setting up the size of the canvas
 function setup() {
-  createCanvas(400, 400); 
- //canvas size is a one time setup
+  createCanvas(300, 300);
 }
 
-
 function draw() {
-  background(0);
-  
-  // Body of car half circle
-  fill(255)
-  let centerX = width / 2; 
-  let centerY = height / 2; // to calculate the center of the canvas
-  let radiusX = 90;
-  let radiusY = 50; // how wide and tall the ellipse is 
-  let startAngle = PI;
-  let endAngle = TWO_PI; // defining the start and end angles for the arc
+  background(0, 0, 51); // dark blue background
 
-  // Draw the half ellipse
-  arc(centerX, centerY, radiusX * 2, radiusY * 2, startAngle, endAngle);
-  
-  
-  
- //wheels 
-  
-  fill(225, 216, 230);
-  ellipse(250,270,40,40);
-  ellipse(150,270,40,40);
-//inside of the wheel
-  fill(0,0,0)
-  ellipse(250, 270, 20, 20);
-  ellipse(150, 270, 20, 20);
-  
-  //body of car
-   
-  fill(225, 216, 230);
-  rect(107, 200, 190, 50,10);
-  
-  //lights 
-   fill(225, 225, 225);
-   ellipse(150, 230, 20, 20);
-   ellipse(250, 230, 20, 20);
-  
-  //s7abat
-   stroke(0)
-   fill(225,225,255);
-  //stroke('rgb(0,0,0)');
- // strokeWeight(3);
-  line(150,200,170,170);
-  line(250,200,265,175);
-  
-  
- 
-  
-  
+  // Draw stars
+  for (let i = 0; i < 50; i++) {
+    stroke(255);//adding white stroke to the stars
+    //random stroke size for diffrent stars size
+    strokeWeight(random(1, 3));
+    //random placment of the point
+    point(random(width), random(height));
+  }
+
+  // Draw the planet surface
+  noStroke();
+  fill(150, 75, 0);
+  ellipse(width / 2, height * 1.5, width * 1.5, height);
+
+  // Alien body
+  translate(width / 2, height / 2);
+  fill(209, 180, 200);
+  beginShape();
+  vertex(-20, -40);
+  bezierVertex(-40, -80, 40, -80, 20, -40);
+  bezierVertex(40, -80, -40, -80, -20, -40);
+  endShape(CLOSE);
+
+  // Alien head
+  fill(51, 255, 153);
+  ellipse(0, -80, 70, 90);
+
+  // Eyes
+  fill(255);
+  ellipse(-20, -100, 20, 20);
+  ellipse(20, -100, 20, 20);
+
+  // Pupils
+  fill(50);
+  ellipse(-20, -100, 10, 11);
+  ellipse(20, -100, 10, 10);
+
+  // Smile
+  noFill();
+  stroke(50);
+  strokeWeight(2);
+  beginShape();
+  vertex(-20, -70);
+  bezierVertex(-10, -60, 10, -60, 20, -70);
+  endShape();
+
+  // Antennae
+  stroke(50);
+  strokeWeight(3);
+  line(-10, -120, -20, -140);
+  line(10, -120, 20, -140);
 }
